35c4727726cd88cb89b0c95f6d31e41711a89013
Herghys <helmi.santosa1@gmail.com>
2022-12-14 11:20:37 +0700

    Fix Lock Data

--- RandomStatsGenerator/Form1.cs
+++ RandomStatsGenerator/Form1.cs
@@ -18,8 +18,12 @@ namespace RandomStatsGenerator
 
 		float total, power;
 
-		float health, attack, defense, agility, accuracy, resistance, critChance, critDamage;
-		double healthP, attackP, defenseP, agilityP, accuracyP, resistanceP, critChanceP, critDamageP;
+		//float health, attack, defense, agility, accuracy, resistance, critChance, critDamage;
+		//double healthP, attackP, defenseP, agilityP, accuracyP, resistanceP, critChanceP, critDamageP;
+
+		float[] statsList= new float[8];
+		double[] statsPList = new double[8];
+		bool[] unlocked = new bool[8];
 
 		public Form1()
 		{
@@ -28,14 +32,24 @@ namespace RandomStatsGenerator
 
 		private void LockCheckBoxChanged(object sender, EventArgs e)
 		{
-			nmcHealth.Enabled = !checkHealth.Checked;
-			nmcAttack.Enabled = !checkAttack.Checked;
-			nmcDefense.Enabled = !checkDefense.Checked;
-			nmcAgility.Enabled = !checkAgility.Checked;
-			nmcAccuracy.Enabled = !checkAccuracy.Checked;
+			nmcHealth.Enabled =	!checkHealth.Checked;
+			nmcAttack.Enabled =	!checkAttack.Checked;
+			nmcDefense.Enabled =!checkDefense.Checked;
+			nmcAgility.Enabled =!checkAgility.Checked;
+			nmcAccuracy.Enabled =!checkAccuracy.Checked;
 			nmcResistance.Enabled = !checkResistance.Checked;
 			nmcCritChance.Enabled = !checkCritChance.Checked;
 			nmcCritDamage.Enabled = !checkCritDamage.Checked;
+
+
+			unlocked[0] =checkHealth.Checked;
+			unlocked[1] =checkAttack.Checked;
+			unlocked[2] =checkDefense.Checked;
+			unlocked[3] =checkAgility.Checked;
+			unlocked[4] =checkAccuracy.Checked;
+			unlocked[5] =checkResistance.Checked;
+			unlocked[6] =checkCritChance.Checked;
+			unlocked[7] =checkCritDamage.Checked;
 		}
 
 		private void OnNumericValueChanged(object sender, EventArgs e)
@@ -56,92 +70,113 @@ namespace RandomStatsGenerator
 		}
 		private void CalculateRandom()
 		{
-			//Random rand = new Random();
-			//var totalP = 0;
-			//total = 0;
+			if (!unlocked[0])
+				statsPList[0]= GetPseudoDoubleWithinRange(0.1, 0.35f);
 
-			healthP = GetPseudoDoubleWithinRange(0.1, 0.35f);
-			attackP = GetPseudoDoubleWithinRange(0.01, 0.15f);
-			defenseP = GetPseudoDoubleWithinRange(0.01, 0.15f);
-			agilityP = GetPseudoDoubleWithinRange(0.01, 0.10f);
-			accuracyP = GetPseudoDoubleWithinRange(0.01, 0.05f);
-			resistanceP = GetPseudoDoubleWithinRange(0.025, 0.10f);
-			critChanceP = GetPseudoDoubleWithinRange(0.01, 0.05f);
-			critDamageP = GetPseudoDoubleWithinRange(0.01, 0.05f);
+			if (!unlocked[1])
+				statsPList[1]= GetPseudoDoubleWithinRange(0.01, 0.15f);
 
-			/*while (total < 95 || total > 100)
-			{
-				critDamageP = rand.Next(1, 5);
-				critChanceP = rand.Next(1, 5);
-				accuracyP = rand.Next(1, 6);
-				resistanceP = rand.Next(1, 6);
-				totalP = (critChanceP + critDamageP + accuracyP + resistanceP);
-				sisa = 100 - totalP;
+			if (!unlocked[2])
+				statsPList[2]= GetPseudoDoubleWithinRange(0.01, 0.15f);
 
-				healthP = rand.Next(1, sisa);
-				totalP = (critChanceP + critDamageP + accuracyP + resistanceP + healthP);
-				sisa = 100 - totalP;
+			if (!unlocked[3])
+				statsPList[3]= GetPseudoDoubleWithinRange(0.01, 0.10f);
 
-				attackP = rand.Next(1, sisa);
-				totalP = (critChanceP + critDamageP + accuracyP + resistanceP + healthP + attackP);
-				sisa = 100 - totalP ;
+			if (!unlocked[4])
+				statsPList[4]= GetPseudoDoubleWithinRange(0.01, 0.05f);
 
-				try
-				{
-					defenseP = rand.Next(1, sisa);
-					totalP = (critChanceP + critDamageP + accuracyP + resistanceP + healthP + attackP + defenseP);
-					sisa = 100 - totalP;
-				}
-				catch { }
+			if (!unlocked[5])
+				statsPList[5] = GetPseudoDoubleWithinRange(0.025, 0.10f);
 
-				try
-				{
-					agilityP = rand.Next(1, sisa);
-					totalP = (critChanceP + critDamageP + accuracyP + resistanceP + healthP + attackP + defenseP + agilityP);
-					sisa = 100 - totalP;
-				}
-				catch { }
+			if (!unlocked[6])
+				statsPList[6] = GetPseudoDoubleWithinRange(0.01, 0.05f);
 
-				
-			}*/
+			if (!unlocked[7])
+				statsPList[7] = GetPseudoDoubleWithinRange(0.01, 0.05f);
 
-			nmcHealth.Text = (healthP * 100).ToString();
-			nmcAttack.Text = (attackP * 100).ToString();
-			nmcDefense.Text =( defenseP * 100).ToString();
-			nmcAgility.Text = (agilityP * 100).ToString();
-			nmcAccuracy.Text = (accuracyP * 100).ToString();
-			nmcResistance.Text = (resistanceP * 100).ToString();
-			nmcCritDamage.Text = (critDamageP * 100).ToString();
-			nmcCritChance.Text = (critChanceP * 100).ToString();
+			CalculateNew();
+			UpdateUI();
+		}
+
+		private void UpdateUI()
+		{
+			nmcHealth.Text = (statsPList[0] * 100).ToString();
+			nmcAttack.Text = (statsPList[1] * 100).ToString();
+			nmcDefense.Text = (statsPList[2] * 100).ToString();
+			nmcAgility.Text = (statsPList[3] * 100).ToString();
+			nmcAccuracy.Text = (statsPList[4] * 100).ToString();
+			nmcResistance.Text = (statsPList[5] * 100).ToString();
+			nmcCritChance.Text = (statsPList[6] * 100).ToString();
+			nmcCritDamage.Text = (statsPList[7] * 100).ToString();
+			
+
+			textHealth.Text = statsList[0].ToString();
+			textAttack.Text = statsList[1].ToString();
+			textDefense.Text = statsList[2].ToString();
+			textAgility.Text = statsList[3].ToString();
+			textAccuracy.Text = statsList[4].ToString();
+			textResistance.Text = statsList[5].ToString();
+			textCritDamage.Text = statsList[6].ToString();
+			textCritChance.Text = statsList[7].ToString();
+			
+			labelTotal.Text = $"Total = {total}%";
+			labelPower.Text = $"Total Power = {power}";
 		}
 
 		private void CalculateNew()
 		{
-			health = (float)Math.Ceiling(nmcHealth.Value * maxValue / 100 * 4);
-			attack = (float)Math.Ceiling(nmcAttack.Value * maxValue / 100 * 4);
-			defense = (float)Math.Ceiling(nmcDefense.Value * maxValue / 100 * 4);
-			agility = (float)Math.Ceiling(nmcAgility.Value * maxValue / 100 * 4);
-			accuracy = (float)Math.Ceiling(nmcAccuracy.Value * maxValue / 100 * 4);
-			resistance = (float)Math.Ceiling(nmcResistance.Value * maxValue / 100 * 4);
-			critChance = (float)Math.Ceiling(nmcCritChance.Value * maxValue / 100 * 4);
-			critDamage = (float)Math.Ceiling(nmcCritDamage.Value * maxValue / 100 * 4);
+			statsList[0] = (float)Math.Ceiling(nmcHealth.Value * maxValue / 100 * 4);
+			statsList[1] = (float)Math.Ceiling(nmcAttack.Value * maxValue / 100 * 4);
+			statsList[2] = (float)Math.Ceiling(nmcDefense.Value * maxValue / 100 * 4);
+			statsList[3] = (float)Math.Ceiling(nmcAgility.Value * maxValue / 100 * 4);
+			statsList[4] = (float)Math.Ceiling(nmcAccuracy.Value * maxValue / 100 * 4);
+			statsList[5] = (float)Math.Ceiling(nmcResistance.Value * maxValue / 100 * 4);
+			statsList[6] = (float)Math.Ceiling(nmcCritChance.Value * maxValue / 100 * 4);
+			statsList[7] = (float)Math.Ceiling(nmcCritDamage.Value * maxValue / 100 * 4);
 
-			textHealth.Text = health.ToString();
-			textAttack.Text = attack.ToString();
-			textDefense.Text = defense.ToString();
-			textAgility.Text = agility.ToString();
-			textAccuracy.Text = accuracy.ToString();
-			textResistance.Text = resistance.ToString();
-			textCritChance.Text = critChance.ToString();
-			textCritDamage.Text = critDamage.ToString();
-
-
-			power = (float)Math.Floor(((float)health * 0.35f * 2) + ((float)attack * 0.15f * 2) + ((float)defense * 0.15f * 2) + ((float)agility * 0.10f * 2) +
-				((float)accuracy * 0.05f * 2) + ((float)resistance * 0.10f * 2) + ((float)critChance * 0.05f * 2) + ((float)critDamage * 0.05f * 2));
+			
+			power = (float)Math.Floor(((float)statsList[0] * 0.35f * 2) + ((float)statsList[1] * 0.15f * 2) + ((float)statsList[2] * 0.15f * 2) + ((float)statsList[3] * 0.10f * 2) +
+				((float)statsList[4] * 0.05f * 2) + ((float)statsList[5] * 0.10f * 2) + ((float)statsList[6] * 0.05f * 2) + ((float)statsList[7] * 0.05f * 2));
 
 			total = (float)(nmcHealth.Value + nmcAttack.Value + nmcDefense.Value + nmcAgility.Value + nmcAccuracy.Value + nmcResistance.Value + nmcCritChance.Value + nmcCritDamage.Value);
-			labelTotal.Text = $"Total = {total}%";
-			labelPower.Text = $"Total Power = {power}";
 		}
 	}
 }
+
+
+
+/*while (total < 95 || total > 100)
+{
+	critDamageP = rand.Next(1, 5);
+	critChanceP = rand.Next(1, 5);
+	accuracyP = rand.Next(1, 6);
+	resistanceP = rand.Next(1, 6);
+	totalP = (critChanceP + critDamageP + accuracyP + resistanceP);
+	sisa = 100 - totalP;
+
+	healthP = rand.Next(1, sisa);
+	totalP = (critChanceP + critDamageP + accuracyP + resistanceP + healthP);
+	sisa = 100 - totalP;
+
+	attackP = rand.Next(1, sisa);
+	totalP = (critChanceP + critDamageP + accuracyP + resistanceP + healthP + attackP);
+	sisa = 100 - totalP ;
+
+	try
+	{
+		defenseP = rand.Next(1, sisa);
+		totalP = (critChanceP + critDamageP + accuracyP + resistanceP + healthP + attackP + defenseP);
+		sisa = 100 - totalP;
+	}
+	catch { }
+
+	try
+	{
+		agilityP = rand.Next(1, sisa);
+		totalP = (critChanceP + critDamageP + accuracyP + resistanceP + healthP + attackP + defenseP + agilityP);
+		sisa = 100 - totalP;
+	}
+	catch { }
+
+
+}*/
